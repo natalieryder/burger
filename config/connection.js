53269320e9d96b6876c479c911c735f7f6e04709
Natalie Ryder <nryder121@pointloma.edu>
2018-02-23 22:55:57 -0800

    created heroku database

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
 var mysql = require("mysql");
 
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
+
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE_NAME,
 });
 
 connection.connect(function(err) {
